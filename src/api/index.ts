1e0d4329c2ffcc5decb149688990e177a04f6029
Shashank Kumar <shashankkumarthakur@gmail.com>
2021-02-19 00:44:23 +0530

    ✨ Add new routes to index

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,13 +1,22 @@
-import { Router, Request, Response } from 'express';
+import express, { Router, Request, Response } from 'express';
 import { routerHandler } from './router';
+
 export default (): Router => {
-  const app = Router();
+  const app = express();
+  const app2 = express();
+  let path = 'C:/URL Short/intachota/src/views';
+  app.set('views', path);
+  app.set('view engine', 'ejs');
 
   //TODO: add routes here...
   app.use('/test', (req: Request, res: Response) => {
     res.send('Test successful');
   });
 
-  app.use('/link', routerHandler());
+  app.use('/link/', routerHandler());
+
+  app.get('*', (req: Request, res: Response) => {
+    res.render('error');
+  });
   return app;
 };
