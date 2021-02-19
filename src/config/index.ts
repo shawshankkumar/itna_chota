908a1fe3d58ed9afdaed222a5c87863f9af7ef35
Shashank Kumar <shashankkumarthakur@gmail.com>
2021-02-20 01:29:13 +0530

    ⚡️ address issue fixed

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,32 +3,32 @@ require('dotenv').config();
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 export default {
-  /**
-   * Port the app should run on
-   */
-  port: parseInt(process.env.PORT) || 5050,
+    /**
+     * Port the app should run on
+     */
+    port: parseInt(process.env.PORT) || 3000,
 
-  /**
-   * Database the app should connect to
-   */
-  databaseURL: process.env.MONGODB_URI,
+    /**
+     * Database the app should connect to
+     */
+    databaseURL: process.env.MONGODB_URI,
 
-  /**
-   * The secret sauce to validate JWT
-   */
-  jwtSecret: process.env.JWT_SECRET,
+    /**
+     * The secret sauce to validate JWT
+     */
+    jwtSecret: process.env.JWT_SECRET,
 
-  /**
-   * Used by Winston logger
-   */
-  logs: {
-    level: process.env.LOG_LEVEL || 'silly',
-  },
+    /**
+     * Used by Winston logger
+     */
+    logs: {
+        level: process.env.LOG_LEVEL || 'silly',
+    },
 
-  /**
-   * API configs
-   */
-  api: {
-    prefix: '/api',
-  },
+    /**
+     * API configs
+     */
+    api: {
+        prefix: '/api',
+    },
 };
