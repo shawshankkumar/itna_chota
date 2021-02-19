b1724dc0b69c9d4ce9d9b12aa9df83559f532f8b
Shashank Kumar <shashankkumarthakur@gmail.com>
2021-02-20 03:36:34 +0530

    🐛 Works for everyport in .env now

--- src/api/router.ts
+++ src/api/router.ts
@@ -1,16 +1,20 @@
 import express, { Router, Request, Response } from 'express';
 import { createLink, fetchLink } from './controller';
-const bodyParser = require('body-parser');
 import config from './../config/index';
 import urljoin from 'url-join';
+
+const bodyParser = require('body-parser');
+
 const app = express();
+
 app.use(bodyParser.urlencoded({ extended: true }));
+
 let path = require('path');
 let dirname = __dirname;
 let l = dirname.length;
 let path3 = dirname.substring(0, l - 10);
 let path1 = path.join(path3, '/src', '/views');
-console.log(path1);
+
 app.set('views', path1);
 
 app.set('view engine', 'ejs');
@@ -20,7 +24,7 @@ export const routerHandler = () => {
         res.render('hein');
     });
     app.get('/', landingPage);
-    app.post('/create', createLinkHandler);
+    app.post('/display', createLinkHandler);
     app.get('/:code', fetchLinkHandler);
     app.get('/display/:code', display);
     return app;
@@ -31,9 +35,9 @@ const createLinkHandler = (req: express.Request, res: express.Response) => {
         .then(code => {
             let portno = config.port;
             let portno1 = portno.toString();
-            let url2 = urljoin('http://localhost:', portno1, '/api/link/display');
+            let url2 = 'http://localhost:' + portno1 + '/api/link/display/';
             console.log(url2);
-            let url = 'http://localhost:3000/api/link/display/' + code;
+            let url = url2 + code;
             console.log(url);
             res.redirect(url);
         })
@@ -43,7 +47,9 @@ const createLinkHandler = (req: express.Request, res: express.Response) => {
 };
 
 const display = (req: express.Request, res: express.Response) => {
-    let url = 'http://localhost:3000/api/link/' + req.params.code;
+    let portno = config.port;
+    let portno1 = portno.toString();
+    let url = 'http://localhost:' + portno1 + '/api/link/' + req.params.code;
     console.log(url);
     res.render('code', { url: url });
 };
@@ -59,5 +65,8 @@ const fetchLinkHandler = (req: express.Request, res: express.Response) => {
 };
 
 const landingPage = (req: express.Request, res: express.Response) => {
-    res.render('index');
+    let portno = config.port;
+    let portno1 = portno.toString();
+    let url = 'http://localhost:' + portno1 + '/api/link/' + req.params.code;
+    res.render('index', { url: url });
 };
